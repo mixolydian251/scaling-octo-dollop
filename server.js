593a64373d2f232d573d848138ed5c98166ab000
jordan <jordanhensley93@gmail.com>
2019-08-16 21:00:56 -0400

    travis config

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-var name = "Jordan";
-
 var db = require("./models");
 
 var app = express();
