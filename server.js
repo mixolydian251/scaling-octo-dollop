b006139f8f12200393782ee019dfb0ae5dbf2c02
jordan <jordanhensley93@gmail.com>
2019-08-16 20:43:27 -0400

    failing test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+var name = "Jordan";
+
 var db = require("./models");
 
 var app = express();
